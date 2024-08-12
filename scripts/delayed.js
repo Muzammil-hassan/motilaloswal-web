08711982aa7156dd7d70df6af4233303a1bb4c71
Muzammil-hassan <muzammil.hassan1@techchefz.com>
2024-08-12 19:11:28 +0530

    fix: header sticky position

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,10 @@
 // add delayed functionality here
+window.addEventListener('scroll', () => {
+  const stickyElement = document.querySelector('.header-wrapper');
+
+  if (window.scrollY > 0) {
+    stickyElement.style.background = 'white';
+  } else {
+    stickyElement.style.background = 'transparent';
+  }
+});
